671f84c309c79fd1d8ebb0f6a532c4c102110512
Achille <achille.mbougueng@chillo.tech>
2024-09-07 07:46:56 +0200

    Projet initial

---
+++ scripts/operations.js
@@ -0,0 +1,21 @@
+var entier;
+entier = 10;
+console.log(typeof entier, entier);
+
+var reel = 10.45;
+console.log(typeof reel, reel);
+
+let chaineDeCaracteres = "Achille";
+console.log(typeof chaineDeCaracteres, chaineDeCaracteres);
+
+const PI = 22/7;
+console.log(typeof PI, PI);
+
+const OTHER_PI = 3.14;
+console.log(OTHER_PI);
+
+const IS_TRAINER = true;
+console.log(typeof IS_TRAINER, IS_TRAINER);
+
+let numbers = [10,3,4,45];
+console.log(typeof numbers, numbers);
