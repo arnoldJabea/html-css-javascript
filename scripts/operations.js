23751a201c7f4560e099fbcb655ca561340b72ff
Achille <achille.mbougueng@chillo.tech>
2024-09-09 07:01:23 +0200

    Chaines de caractères

--- scripts/operations.js
+++ scripts/operations.js
@@ -13,14 +13,33 @@ function calculer(event) {
   const selectedOperation = operation.value;
   console.log(valueOfSecondNumberInput);
   let result = 0;
-  if (selectedOperation == 1) {
+
+  console.log(typeof selectedOperation);
+
+  if (Number(selectedOperation) === 1) {
     result = 1 * valueOfSecondNumberInput + 1 * valueOfFirstNumberInput;
-  } else if (selectedOperation == 2) {
+  } else if (Number(selectedOperation) === 2) {
     result = Number(valueOfSecondNumberInput) * Number(valueOfFirstNumberInput);
   } else {
     result = Number(valueOfSecondNumberInput) % Number(valueOfFirstNumberInput);
   }
-  console.log(result);
+  console.log("result " + result);
 
-  document.getElementById("result").innerHTML = result;
+  const name = document.getElementById("name");
+  const valueOfNameInput = name.value;
+  console.log(`Name: ${valueOfNameInput}`);
+
+  // Initiales
+  const fistNameAndLastname = valueOfNameInput.split(" ");
+  const initials = `${fistNameAndLastname[0][0]}${fistNameAndLastname[0][1]}`;
+
+  // concatenation
+  let nameAndResult = valueOfNameInput + " " + result;
+  nameAndResult = `${initials.toUpperCase()} ${result}`;
+
+  // longueur d'une chaine;
+  const length = nameAndResult.length;
+  console.log(length);
+
+  document.getElementById("result").innerHTML = nameAndResult;
 }
